435dd3f9f99525b78ba979558ed9506c8cca75a7
rhaskia <rhaskiagamedev@gmail.com>
2024-03-27 17:15:59 +1300

    drawer struct

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,18 @@ use crossterm::event::{KeyEventKind, KeyModifiers};
 use crossterm::{
     event::{poll, read, Event, KeyCode, KeyEvent},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use std::io::Write;
 use std::io::{self, stdout};
 
 mod renderer;
-use renderer::render_md;
+use renderer::Drawer;
 
 fn main() -> io::Result<()> {
-    let mut stdout = stdout();
     let mut current = String::new();
+    let mut drawer = Drawer::new();
 
-    enable_raw_mode()?; // Enable raw mode to capture input without buffering
-    execute!(stdout, EnterAlternateScreen);
-
-    println!("welcome");
-    print!("\x1b[2K\r λ ");
-    stdout.flush();
+    drawer.alt_screen(true);
 
     loop {
         if poll(std::time::Duration::from_millis(50))? {
@@ -31,15 +25,18 @@ fn main() -> io::Result<()> {
                     KeyCode::Char('d') if modifiers == CTRL => break,
                     KeyCode::Char('c') if modifiers == CTRL => break,
 
+                    KeyCode::Enter => {
+                        current.push('\n');
+                        drawer.render_md(&current);
+                    }
+
                     KeyCode::Backspace => { 
                         current.pop();
-                        render_md(&current);
-                        stdout.flush();
+                        drawer.render_md(&current);
                     },
                     KeyCode::Char(c) => {
                         current.push(c);
-                        render_md(&current);
-                        stdout.flush();
+                        drawer.render_md(&current);
                     }
                     _ => (),
                 }
@@ -47,8 +44,7 @@ fn main() -> io::Result<()> {
         }
     }
 
-    disable_raw_mode()?; // Disable raw mode before exiting
-    execute!(stdout, LeaveAlternateScreen);
+    drawer.alt_screen(false);
 
     Ok(())
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,7 +1,47 @@
 use markdown::*;
+use std::io::Write;
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType, 
+    disable_raw_mode, enable_raw_mode,
+    EnterAlternateScreen, LeaveAlternateScreen
+};
+use crossterm::cursor::MoveTo;
 
-pub fn render_md(file: &String) {
-    let options = ParseOptions::default();
-    let tree = to_mdast(file, &options);
-    println!("{tree:?}");
+pub struct Drawer {
+    out: std::io::Stdout,    
+}
+
+impl Drawer {
+    pub fn render_md(&mut self, file: &String) {
+        let options = ParseOptions::default();
+        let tree = match to_mdast(file, &options) {
+            Ok(t) => t,
+            Err(err) => {
+                eprintln!("Markdown failed to render @{err:?}");
+                return;
+            }
+        };
+
+        execute!(&self.out, Clear(ClearType::All), MoveTo(0, 0));
+
+        println!("{tree:?}");
+
+        self.out.flush();
+    }
+
+    pub fn alt_screen(&mut self, active: bool) {
+        if active {
+            enable_raw_mode().unwrap(); // Enable raw mode to capture input without buffering
+            execute!(&self.out, EnterAlternateScreen);
+        } else {
+            execute!(&self.out, LeaveAlternateScreen);
+            disable_raw_mode();
+        }
+    } 
+
+    pub fn new() -> Self {
+        Drawer { 
+            out: std::io::stdout(),
+        }
+    }
 }
